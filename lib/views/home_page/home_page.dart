a8220243e243e2d0946b61b3c516addd4921486e
Sahan Perera <sahanperera@arimaclanka.com>
2021-04-15 13:01:19 +0530

    window buttons ok

--- lib/views/home_page/home_page.dart
+++ lib/views/home_page/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:communication/helpers/images.dart';
 import 'package:communication/scoped_model/main.dart';
 import 'package:communication/views/auth/change_password.dart';
@@ -21,26 +22,71 @@ enum HomePagePopups {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+  HomePage({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     Messages.setContext(context);
     return _buildBody(context);
   }
 
+  final minButtonColors = WindowButtonColors(
+    iconNormal: Colors.white,
+    mouseOver: Color(0xFFF6A00C),
+    mouseDown: Color(0xFF805306),
+    iconMouseOver: Colors.white,
+  );
+
+  final maxButtonColors = WindowButtonColors(
+    iconNormal: Colors.white,
+    mouseOver: Colors.green,
+    mouseDown: Color(0xFF805306),
+    iconMouseOver: Colors.white,
+  );
+
+  final closeButtonColors = WindowButtonColors(
+    iconNormal: Colors.white,
+    mouseOver: Color(0xFFD32F2F),
+    mouseDown: Color(0xFFB71C1C),
+    iconMouseOver: Colors.white,
+  );
+
   Widget _buildBody(BuildContext context) {
     return ScopedModelDescendant<MainModel>(
       builder: (context, child, model) => Scaffold(
         appBar: AppBar(
-          title: Text('Hello, ${model.user != null ? model.user.name : ''}'),
+          // title: Text('Hello, ${model.user != null ? model.user.name : ''}'),
           actions: [
-            Container(
-              margin: const EdgeInsets.only(right: 20),
-              child: IconButton(
-                  icon: Icon(Icons.exit_to_app),
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, '/landing');
-                  }),
+            SizedBox(
+              width: 100,
+            ),
+            Expanded(
+              child: MoveWindow(
+                child: Center(
+                  widthFactor: 1,
+                  child: Text(
+                    'Hello, ${model.user != null ? model.user.name : ''}',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+            _myWindowButton(
+              child: MinimizeWindowButton(
+                colors: minButtonColors,
+                animate: true,
+              ),
+            ),
+            _myWindowButton(
+              child: MaximizeWindowButton(
+                colors: maxButtonColors,
+                animate: true,
+              ),
+            ),
+            _myWindowButton(
+              child: CloseWindowButton(
+                colors: closeButtonColors,
+                animate: true,
+              ),
             )
           ],
         ),
@@ -51,6 +97,20 @@ class HomePage extends StatelessWidget {
             children: [SalesPage(), _buildPopup(context)],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _myWindowButton({@required Widget child}) {
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(40),
+            border: Border.all(color: Colors.white.withOpacity(0.8), width: 2)),
+        margin: EdgeInsets.symmetric(horizontal: 3),
+        height: 40,
+        width: 40,
+        child: ClipRRect(borderRadius: BorderRadius.circular(40), child: child),
       ),
     );
   }
